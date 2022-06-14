1d06ee4ed143e85a7a3744fa9c6164a20f9d7d4a
Peter <peter_s_96@live.com>
2022-06-13 19:20:46 -0700

    commit

---
+++ api/index.js
@@ -0,0 +1,11 @@
+// api/index.js
+const express = require('express');
+const apiRouter = express.Router();
+
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
+
+module.exports = apiRouter;
+module.exports = usersRouter;
+module.exports = postsRouter;
+module.exports = tagsRouter;
